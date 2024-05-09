f390bc919b7dfd46770a1a8324910c6b155da040
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-05-09 20:39:18 +0530

    build: Add attendee-service for Attendee related tasks.

---
+++ backend/services/attendee-service/build.gradle.kts
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) PU Convocation Management System Authors
+ *
+ * This software is owned by PU Convocation Management System Authors.
+ * No part of the software is allowed to be copied or distributed
+ * in any form. Any attempt to do so will be considered a violation
+ * of copyright law.
+ *
+ * This software is protected by copyright law and international
+ * treaties. Unauthorized copying or distribution of this software
+ * is a violation of these laws and could result in severe penalties.
+ */
+
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.24"
+    id("io.ktor.plugin") version "2.3.10"
+}
+
+group = "com.puconvocation"
+version = "0.0.1"
+
+application {
+    mainClass.set("com.puconvocation.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-compression-jvm")
+    implementation("io.ktor:ktor-server-cors-jvm")
+    implementation("io.ktor:ktor-server-default-headers-jvm")
+    implementation("io.ktor:ktor-server-call-logging-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-serialization-gson-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+}
---
+++ backend/services/attendee-service/src/main/kotlin/com/puconvocation/plugins/Serialization.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) PU Convocation Management System Authors
+ *
+ * This software is owned by PU Convocation Management System Authors.
+ * No part of the software is allowed to be copied or distributed
+ * in any form. Any attempt to do so will be considered a violation
+ * of copyright law.
+ *
+ * This software is protected by copyright law and international
+ * treaties. Unauthorized copying or distribution of this software
+ * is a violation of these laws and could result in severe penalties.
+ */
+
+package com.puconvocation.plugins
+
+import io.ktor.serialization.gson.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+}
