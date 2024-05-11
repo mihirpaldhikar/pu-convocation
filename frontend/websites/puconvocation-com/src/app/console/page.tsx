35f5212b8584f7c3f11c85529bd662e0b322d92d
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-05-11 23:03:30 +0530

    feat (puconvocation-com): Add Basic console page.

---
+++ frontend/websites/puconvocation-com/src/app/console/page.tsx
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) PU Convocation Management System Authors
+ *
+ * This software is owned by PU Convocation Management System Authors.
+ * No part of the software is allowed to be copied or distributed
+ * in any form. Any attempt to do so will be considered a violation
+ * of copyright law.
+ *
+ * This software is protected by copyright law and international
+ * treaties. Unauthorized copying or distribution of this software
+ * is a violation of these laws and could result in severe penalties.
+ */
+
+import { JSX } from "react";
+
+export default function ConsolePage(): JSX.Element {
+  return (
+    <div className={"min-h-screen flex"}>
+      <div className={"m-auto"}>
+        <div>
+          <h1 className={"font-bold text-2xl"}>PU Convocation Console</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
