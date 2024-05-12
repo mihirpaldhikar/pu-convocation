4a424f6b69cb104255be1e63624082cec2629ea0
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-05-12 15:57:38 +0530

    feat (puconvocation-com): Add the ability to automatically refresh the security tokens.

--- frontend/websites/puconvocation-com/src/middleware.ts
+++ frontend/websites/puconvocation-com/src/middleware.ts
@@ -15,8 +15,23 @@ import { NextRequest, NextResponse } from "next/server";
 
 const protectedRoutes: Array<string> = ["/console"];
 
+function parseCookie(cookieArray: string[]): Record<string, string> {
+  const cookieObject: Record<string, string> = {};
+
+  for (const cookieString of cookieArray) {
+    const [key, ...valueParts] = cookieString.split("=");
+    const value = valueParts.join("=");
+
+    if (key.toLowerCase() !== "httponly") {
+      cookieObject[key] = value;
+    }
+  }
+
+  return cookieObject;
+}
+
 export default async function middleware(req: NextRequest) {
-  const authResponse = await fetch(
+  const authenticationResponse = await fetch(
     `${process.env.NEXT_PUBLIC_AUTH_SERVICE_URL}/accounts/`,
     {
       credentials: "same-origin",
@@ -27,16 +42,65 @@ export default async function middleware(req: NextRequest) {
     },
   );
 
-  if (req.nextUrl.pathname.startsWith("/auth") && authResponse.status === 200) {
+  if (
+    req.nextUrl.pathname.startsWith("/auth") &&
+    authenticationResponse.status === 200
+  ) {
     const absoluteURL = new URL("/console", req.nextUrl.origin);
     return NextResponse.redirect(absoluteURL.toString());
   }
 
   if (
-    authResponse.status !== 200 &&
+    authenticationResponse.status !== 200 &&
     protectedRoutes.includes(req.nextUrl.pathname)
   ) {
-    const absoluteURL = new URL("/auth/signin", req.nextUrl.origin);
-    return NextResponse.redirect(absoluteURL.toString());
+    if (req.cookies.has("__puc_rt__") && !req.cookies.has("__puc_at__")) {
+      const refreshSecurityTokenResponse = await fetch(
+        `${process.env.NEXT_PUBLIC_AUTH_SERVICE_URL}/accounts/refresh`,
+        {
+          credentials: "same-origin",
+          method: "POST",
+          headers: {
+            Cookie: req.cookies.toString(),
+          },
+        },
+      );
+
+      if (refreshSecurityTokenResponse.status !== 200) {
+        const absoluteURL = new URL("/auth/signin", req.nextUrl.origin);
+        return NextResponse.redirect(absoluteURL.toString());
+      }
+
+      const cookies = refreshSecurityTokenResponse.headers
+        .getSetCookie()
+        .toString()
+        .split(", __");
+
+      const nextResponse = NextResponse.next();
+
+      const authorizationTokenCookie = parseCookie(cookies[0].split(";"));
+      const refreshTokenCookie = parseCookie(cookies[1].split(";"));
+
+      nextResponse.cookies
+        .set("__puc_at__", authorizationTokenCookie["__puc_at__"], {
+          expires: new Date(authorizationTokenCookie["Expires"]),
+          domain: authorizationTokenCookie["Domain"],
+          path: authorizationTokenCookie["Path"],
+          sameSite: "lax",
+          httpOnly: true,
+        })
+        .set("__puc_rt__", refreshTokenCookie["puc_rt__"], {
+          expires: new Date(refreshTokenCookie["Expires"]),
+          domain: refreshTokenCookie["Domain"],
+          path: refreshTokenCookie["Path"],
+          sameSite: "lax",
+          httpOnly: true,
+        });
+
+      return nextResponse;
+    } else {
+      const absoluteURL = new URL("/auth/signin", req.nextUrl.origin);
+      return NextResponse.redirect(absoluteURL.toString());
+    }
   }
 }
