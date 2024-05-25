5e934117225ca8092d1446fdff238ef08236a04f
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-05-25 10:28:50 +0530

    refactor (puconvocation-com): Refactor Footer for easier management of the footer content.

--- frontend/websites/puconvocation-com/src/components/footer.tsx
+++ frontend/websites/puconvocation-com/src/components/footer.tsx
@@ -11,111 +11,131 @@
  * is a violation of these laws and could result in severe penalties.
  */
 
-import { JSX } from "react";
+import { Fragment, JSX } from "react";
 import { Flags } from "@components/index";
 import Link from "next/link";
 
+const credits: Array<{
+  name: string;
+  credits: Array<{
+    name: string;
+    link: string;
+    bold: boolean;
+    underline: boolean;
+  }>;
+}> = [
+  {
+    name: "Developed By",
+    credits: [
+      {
+        name: "Suhani Shah",
+        link: "https://www.linkedin.com/in/suhani-shah-o13",
+        bold: true,
+        underline: true,
+      },
+      {
+        name: "Mihir Paldhikar",
+        link: "https://mihirpaldhikar.com",
+        bold: true,
+        underline: true,
+      },
+    ],
+  },
+  {
+    name: "Coordinated By",
+    credits: [
+      {
+        name: "Prof. Mohit Rathod",
+        link: "https://www.linkedin.com/in/er-mohit-68a447a0",
+        bold: false,
+        underline: false,
+      },
+      {
+        name: "Manish Rahevar",
+        link: "https://www.linkedin.com/in/manish-rahevar-b08a87108",
+        bold: false,
+        underline: false,
+      },
+    ],
+  },
+  {
+    name: "Guided By",
+    credits: [
+      {
+        name: "Dr. Swapnil M Parikh",
+        link: "https://www.linkedin.com/in/dr-swapnil-parikh-43a90715",
+        bold: false,
+        underline: false,
+      },
+      {
+        name: "Prof. Sumitra Menaria",
+        link: "https://www.linkedin.com/in/sumitra-menaria-0bab23123",
+        bold: false,
+        underline: false,
+      },
+    ],
+  },
+];
+
 export default function Footer(): JSX.Element {
   return (
-    <footer className={"h-[750px] w-full bg-red-100/40 md:h-[580px]"}>
-      <div className={"relative z-0 flex justify-end"}>
-        <div className={"mt-[-45px] h-full md:mt-[-85px]"}>
-          <Flags />
-        </div>
-        <div className={"absolute inset-0 z-10"}>
-          <div>
-            <div
-              className={
-                "flex h-full flex-col space-y-7  px-4 py-20 md:px-20  md:py-12"
-              }
-            >
-              <div className={"space-y-3"}>
-                <h4 className={"text-lg font-bold"}>Who We Are</h4>
-                <p className={"w-full font-medium md:w-1/2"}>
-                  Parul University - Gujarat’s leading private university having
-                  the foundation of its first Institution laid in 1993 as Parul
-                  Group of Institutes, and later established and incorporated as
-                  Parul University in 2015 under the Gujarat Private
-                  Universities (Second Amendment ) Act of 2009.
-                </p>
-              </div>
-              <div className={"space-y-3"}>
-                <h4 className={"text-lg font-bold"}>Credits</h4>
-                <p className={"font-medium"}>
-                  Developed By{" "}
-                  <Link
-                    href={"https://www.linkedin.com/in/suhani-shah-o13/"}
-                    target={"_blank"}
-                    className={"font-bold text-primary underline"}
-                  >
-                    Suhani Shah
-                  </Link>{" "}
-                  &{" "}
-                  <Link
-                    href={"https://mihirpaldhikar.com"}
-                    target={"_blank"}
-                    className={"font-bold text-primary underline"}
-                  >
-                    Mihir Paldhikar
-                  </Link>
-                </p>
-                <p className={"font-medium"}>
-                  Coordinated By{" "}
-                  <Link
-                    href={"https://www.linkedin.com/in/er-mohit-68a447a0"}
-                    target={"_blank"}
-                    className={"font-medium text-primary"}
-                  >
-                    Prof. Mohit Rathod
-                  </Link>{" "}
-                  ,{" "}
-                  <Link
-                    href={
-                      "https://www.linkedin.com/in/manish-rahevar-b08a87108"
-                    }
-                    target={"_blank"}
-                    className={"font-medium text-primary"}
-                  >
-                    Prof. Manish Rahevar
-                  </Link>
-                </p>
-                <p className={"font-medium"}>
-                  Guided By{" "}
-                  <Link
-                    href={
-                      "https://www.linkedin.com/in/dr-swapnil-parikh-43a90715"
-                    }
-                    target={"_blank"}
-                    className={"font-medium text-primary"}
-                  >
-                    Dr. Swapnil M Parikh
-                  </Link>
-                  ,{" "}
-                  <Link
-                    href={
-                      "https://www.linkedin.com/in/sumitra-menaria-0bab23123"
-                    }
-                    target={"_blank"}
-                    className={"font-medium text-primary"}
-                  >
-                    Prof. Sumitra Menaria
-                  </Link>
-                </p>
-              </div>
-            </div>
-            <div className={"flex flex-col items-center space-y-5"}>
-              <p className={"text-center text-xs"}>
-                &copy; {new Date().getFullYear()} CSE Department, Parul
-                Institute of Technology, FET, Parul University
+    <footer className={"relative z-0 bg-primary-foreground"}>
+      <div className={"absolute -top-[2.8rem] right-0 md:-top-[5.3rem]"}>
+        <Flags />
+      </div>
+      <div className={"space-y-10 px-5 pb-10 pt-24 md:px-10 md:pt-10"}>
+        <section className={"space-y-3"}>
+          <h3 className={"text-xl font-bold"}>We We Are</h3>
+          <p className={"w-full font-medium md:w-2/5"}>
+            Parul University - Gujarat’s leading private university having the
+            foundation of its first Institution laid in 1993 as Parul Group of
+            Institutes, and later established and incorporated as Parul
+            University in 2015 under the Gujarat Private Universities (Second
+            Amendment ) Act of 2009.
+          </p>
+        </section>
+        <section className={"space-y-3"}>
+          <h3 className={"text-xl font-bold"}>Credits</h3>
+          {credits.map((credit, i) => {
+            return (
+              <p key={i} className={"font-medium"}>
+                {credit.name}{" "}
+                {credit.credits.map((c, j) => {
+                  return (
+                    <Fragment key={j}>
+                      <Link
+                        href={c.link}
+                        target={"_blank"}
+                        className={`
+                        ${c.bold ? "font-bold" : "font-medium"} 
+                        ${c.underline ? "underline" : "no-underline"} 
+                        text-primary `}
+                      >
+                        {c.name}
+                      </Link>
+                      {j < credit.credits.length - 1 ? ", " : " "}
+                    </Fragment>
+                  );
+                })}
               </p>
-              <Link href={"https://paruluniversity.ac.in"} target={"_blank"}>
-                <h1 className={"text-4xl font-black text-black/30 md:text-8xl"}>
-                  <span className={"text-primary/30"}>Parul</span> University
-                </h1>
-              </Link>
-            </div>
-          </div>
-        </div>
+            );
+          })}
+        </section>
+        <section className={"space-y-10 text-center"}>
+          <p className={"text-xs"}>
+            &copy; 2024 CSE Department, Parul Institute of Technology, FET,
+            Parul University
+          </p>
+          <Link
+            href={"https://paruluniversity.ac.in"}
+            target={"_blank"}
+            className={
+              "block text-4xl font-black opacity-30 md:text-6xl lg:text-8xl"
+            }
+          >
+            <span className={"text-primary"}>Parul</span> University
+          </Link>
+        </section>
       </div>
     </footer>
   );
