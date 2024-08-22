60adee0f8dd3525212bd7769fcc9622458214cd1
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-08-23 00:01:51 +0530

    feat (puconvocation-com): Add QR Code with new UI to verify and validate attendance and degree certificate transaction.

--- frontend/websites/puconvocation-com/src/components/ticket.tsx
+++ frontend/websites/puconvocation-com/src/components/ticket.tsx
@@ -10,50 +10,51 @@
  * treaties. Unauthorized copying or distribution of this software
  * is a violation of these laws and could result in severe penalties.
  */
-
 import { Attendee } from "@dto/index";
-import { StarIcon } from "@heroicons/react/24/outline";
 import { JSX } from "react";
+import QRCode from "react-qr-code";
 
 interface TicketProps {
   attendee: Attendee;
-  className?: string;
 }
 
-export default function Ticket({
-  attendee,
-  className,
-}: TicketProps): JSX.Element {
+export default function Ticket({ attendee }: TicketProps): JSX.Element {
   return (
     <div
       className={
-        "w-full rounded-md border-2 border-dotted border-yellow-400 bg-yellow-50 px-2 py-3 text-black md:w-[350px]"
+        "flex h-fit w-full flex-col rounded-xl border-2 border-gray-300 bg-white md:w-3/4"
       }
     >
-      <div className={"space-y-3 border-b border-yellow-400"}>
-        <div className={"flex justify-between text-xs font-semibold"}>
-          <div className={"flex items-center"}>
-            <div className={"mr-2 w-5"}>
-              <StarIcon />
-            </div>
-            <h3>CRR: {attendee.convocationId}</h3>
+      <div className={"flex justify-between px-4 pb-2 pt-5"}>
+        <div className={"flex flex-col"}>
+          <h6 className={"text-xs font-medium"}>{attendee.enrollmentNumber}</h6>
+          <h3 className={"pt-4 text-xl font-bold"}>{attendee.studentName}</h3>
+          <h4>{attendee.department}</h4>
+        </div>
+        <div>
+          <div
+            className={"flex flex-col items-center justify-center space-y-2"}
+          >
+            <QRCode size={100} value={attendee.verificationToken} />
+            <h6 className={"text-xs"}>{attendee.convocationId}</h6>
           </div>
-          <h3>ERN: {attendee.enrollmentNumber}</h3>
         </div>
-        <h2 className={"text-lg font-bold"}>{attendee.studentName}</h2>
       </div>
-      <div className={"flex items-center justify-evenly pt-3"}>
-        <div className={"flex flex-col items-center"}>
-          <h2 className={"text-xl font-bold"}>{attendee.enclosure}</h2>
-          <h3 className={"text-xs font-semibold"}>Enclosure</h3>
-        </div>
-        <div className={"flex flex-col items-center"}>
-          <h2 className={"text-xl font-bold"}>{attendee.row}</h2>
-          <h3 className={"text-xs font-semibold"}>Row</h3>
-        </div>
-        <div className={"flex flex-col items-center"}>
-          <h2 className={"text-xl font-bold"}>{attendee.seat}</h2>
-          <h3 className={"text-xs font-semibold"}>Seat</h3>
+      <hr className={"h-0.5 bg-gray-300"} />
+      <div className={"flex items-center justify-center py-4"}>
+        <div className={"flex w-fit space-x-10"}>
+          <div className={"text-center"}>
+            <h5 className={"text-lg font-bold"}>{attendee.enclosure}</h5>
+            <h6 className={"font-semibold"}>Enclosure</h6>
+          </div>
+          <div className={"px-3 text-center"}>
+            <h5 className={"text-lg font-bold"}>{attendee.row}</h5>
+            <h6 className={"font-semibold"}>Row</h6>
+          </div>
+          <div className={"px-3 text-center"}>
+            <h5 className={"text-lg font-bold"}>{attendee.seat}</h5>
+            <h6 className={"font-semibold"}>Seat</h6>
+          </div>
         </div>
       </div>
     </div>
