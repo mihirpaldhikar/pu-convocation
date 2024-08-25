32078e9a8470ae8c53ad03da2113080dae960812
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-08-26 00:14:02 +0530

    feat (puconvocation-com): Add ability to render Login or Console Link based on current auth status in navbar_menu.tsx

--- frontend/websites/puconvocation-com/src/components/navbar_menu.tsx
+++ frontend/websites/puconvocation-com/src/components/navbar_menu.tsx
@@ -13,19 +13,67 @@
 
 "use client";
 
-import { JSX } from "react";
-import { UserCircleIcon } from "@heroicons/react/24/outline";
+import { JSX, useEffect } from "react";
+import { UserCircleIcon, WindowIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { useAuth } from "../providers/AuthProvider";
+import { StatusCode } from "@enums/StatusCode";
+import { AuthService } from "@services/index";
+
+const authService = new AuthService();
 
 export default function NavbarMenu(): JSX.Element {
+  const { state, dispatch } = useAuth();
+  const account = state.account;
+
+  useEffect(() => {
+    dispatch({
+      type: "LOADING",
+      payload: {
+        loading: true,
+      },
+    });
+    if (state.account === null) {
+      authService.getAccount().then((res) => {
+        if (
+          res.statusCode === StatusCode.SUCCESS &&
+          "payload" in res &&
+          typeof res.payload === "object"
+        ) {
+          dispatch({
+            type: "SET_ACCOUNT",
+            payload: {
+              account: res.payload,
+            },
+          });
+        }
+      });
+    }
+    setTimeout(() => {
+      dispatch({
+        type: "LOADING",
+        payload: {
+          loading: false,
+        },
+      });
+    }, 500);
+  }, [dispatch, state.account]);
+
   return (
-    <nav className="flex items-center space-x-5">
+    <nav className={`flex items-center space-x-5`}>
       <Link
         href={"/authenticate"}
-        className={`flex items-center rounded-2xl bg-black px-4 py-2 text-white`}
+        className={`${!state.loading && account === null ? "flex" : "hidden"} items-center rounded-2xl bg-black px-4 py-2 text-white`}
       >
         <UserCircleIcon className={"mr-2 size-5"} />
         <span className="mr-2">Login</span>
+      </Link>
+      <Link
+        href={"/console"}
+        className={`${!state.loading && account !== null ? "flex" : "hidden"} items-center rounded-2xl bg-black px-4 py-2 text-white`}
+      >
+        <WindowIcon className={"mr-2 size-5"} />
+        <span className="mr-2">Console</span>
       </Link>
     </nav>
   );
