f5022aac5ebdd10a031ba49efa8e8a04d3113e56
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-08-26 14:35:19 +0530

    refactor (puconvocation-com): Refactor Manage Account Page to use data provided by AuthProvider instead of fetching data from auth server.

--- frontend/websites/puconvocation-com/src/app/console/account/page.tsx
+++ frontend/websites/puconvocation-com/src/app/console/account/page.tsx
@@ -11,44 +11,21 @@
  * is a violation of these laws and could result in severe penalties.
  */
 "use client";
-import { Fragment, JSX, useEffect, useState } from "react";
+import { Fragment, JSX } from "react";
 import { AuthService } from "@services/index";
-import { Account } from "@dto/index";
 import { StatusCode } from "@enums/StatusCode";
 import { ProgressBar } from "@components/index";
 import { Button } from "@components/ui";
 import { useToast } from "@hooks/use-toast";
+import { useAuth } from "@providers/AuthProvider";
 
 const authService = new AuthService();
 
 export default function AccountPage(): JSX.Element {
   const { toast } = useToast();
+  const { state } = useAuth();
 
-  const [account, setAccount] = useState<Account | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string>("");
-
-  useEffect(() => {
-    authService.getAccount().then((response) => {
-      if (
-        response.statusCode === StatusCode.SUCCESS &&
-        "payload" in response &&
-        typeof response.payload === "object"
-      ) {
-        setAccount(response.payload);
-      } else if (
-        response.statusCode === StatusCode.FAILURE &&
-        "message" in response
-      ) {
-        setError(response.message);
-      } else {
-        setError("An unknown error occurred!");
-      }
-      setLoading(false);
-    });
-  }, []);
-
-  if (loading) {
+  if (state.loading) {
     return (
       <section className={"flex min-h-dvh"}>
         <div className="m-auto flex items-center justify-center space-y-5">
@@ -58,39 +35,32 @@ export default function AccountPage(): JSX.Element {
     );
   }
 
-  if (account !== null) {
-    return (
-      <Fragment>
-        <section className={"space-y-5"}>
-          <h2 className={"text-2xl font-bold"}>Hello, {account.displayName}</h2>
-          <Button
-            onClick={async () => {
-              const response = await authService.registerPasskey(account.uuid);
-              if ("message" in response) {
-                toast({
-                  title:
-                    response.statusCode === StatusCode.PASSKEY_REGISTERED
-                      ? "Success"
-                      : "Failure",
-                  description: response.message,
-                  duration: 5000,
-                });
-              }
-            }}
-          >
-            Register New Passkey
-          </Button>
-        </section>
-      </Fragment>
-    );
-  }
-
   return (
-    <section className={"flex min-h-dvh"}>
-      <div className="m-auto flex items-center justify-center space-y-5">
-        <h3>Cannot fetch account</h3>
-        <p>{error}</p>
-      </div>
-    </section>
+    <Fragment>
+      <section className={"space-y-5"}>
+        <h2 className={"text-2xl font-bold"}>
+          Hello, {state.account?.displayName}
+        </h2>
+        <Button
+          onClick={async () => {
+            const response = await authService.registerPasskey(
+              state.account?.uuid!!,
+            );
+            if ("message" in response) {
+              toast({
+                title:
+                  response.statusCode === StatusCode.PASSKEY_REGISTERED
+                    ? "Success"
+                    : "Failure",
+                description: response.message,
+                duration: 5000,
+              });
+            }
+          }}
+        >
+          Register New Passkey
+        </Button>
+      </section>
+    </Fragment>
   );
 }
