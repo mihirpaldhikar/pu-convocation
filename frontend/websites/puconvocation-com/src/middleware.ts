ea51fac84a22c909e591cc15fc9110f6a52285e3
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-08-27 17:52:32 +0530

    refactor (puconvocation-com): Refactor middleware.ts by removing logic to refresh security tokens. Issuing new tokens if authorization token is expired is now handled by auth-service.

--- frontend/websites/puconvocation-com/src/middleware.ts
+++ frontend/websites/puconvocation-com/src/middleware.ts
@@ -13,33 +13,18 @@
 
 import { NextRequest, NextResponse } from "next/server";
 
-const protectedRoutes: Array<string> = [
-  "/console",
-  "/console/account",
-  "/console/account/new",
-  "/console/scan",
-  "/console/verify",
-];
-
-function parseCookie(cookieArray: string[]): Record<string, string> {
-  const cookieObject: Record<string, string> = {};
-
-  for (const cookieString of cookieArray) {
-    const [key, ...valueParts] = cookieString.split("=");
-    const value = valueParts.join("=");
-
-    if (key.toLowerCase() !== "httponly") {
-      cookieObject[key] = value;
-    }
-  }
-
-  return cookieObject;
-}
+const protectedRoutes: Array<string> = ["/console"];
 
 export default async function middleware(req: NextRequest) {
+  const pathName = req.nextUrl.pathname;
+
+  if (pathName.includes("_next") || pathName.includes("/favicon.ico")) {
+    return;
+  }
+
   if (
-    req.nextUrl.pathname.includes("/authenticate") ||
-    req.nextUrl.pathname.includes("/console")
+    pathName.includes("/authenticate") ||
+    pathName.includes(protectedRoutes.join(" | "))
   ) {
     const authenticationResponse = await fetch(
       `${process.env.NEXT_PUBLIC_AUTH_SERVICE_URL}/accounts/`,
@@ -53,81 +38,19 @@ export default async function middleware(req: NextRequest) {
     );
 
     if (
-      req.nextUrl.pathname.startsWith("/authenticate") &&
-      authenticationResponse.status === 200
+      authenticationResponse.status !== 200 &&
+      !pathName.includes("/authenticate")
     ) {
-      const absoluteURL = new URL("/console", req.nextUrl.origin);
+      const absoluteURL = new URL("/authenticate", req.nextUrl.origin);
       return NextResponse.redirect(absoluteURL.toString());
     }
 
     if (
-      authenticationResponse.status !== 200 &&
-      protectedRoutes.includes(req.nextUrl.pathname)
+      authenticationResponse.status === 200 &&
+      pathName.includes("/authenticate")
     ) {
-      if (
-        authenticationResponse.status === 403 &&
-        protectedRoutes.includes(req.nextUrl.pathname)
-      ) {
-        const absoluteURL = new URL("/authenticate", req.nextUrl.origin);
-        return NextResponse.redirect(absoluteURL.toString());
-      }
-
-      if (req.cookies.has("__puc_rt__") && !req.cookies.has("__puc_at__")) {
-        const refreshSecurityTokenResponse = await fetch(
-          `${process.env.NEXT_PUBLIC_AUTH_SERVICE_URL}/accounts/refresh`,
-          {
-            credentials: "same-origin",
-            method: "POST",
-            headers: {
-              Cookie: req.cookies.toString(),
-            },
-          },
-        );
-
-        if (refreshSecurityTokenResponse.status !== 200) {
-          const absoluteURL = new URL("/authenticate", req.nextUrl.origin);
-          return NextResponse.redirect(absoluteURL.toString());
-        }
-
-        const cookies = refreshSecurityTokenResponse.headers
-          .getSetCookie()
-          .toString()
-          .split(", __");
-
-        const nextResponse = NextResponse.next();
-
-        const authorizationTokenCookie = parseCookie(cookies[0].split(";"));
-        const refreshTokenCookie = parseCookie(cookies[1].split(";"));
-
-        nextResponse.cookies
-          .set("__puc_at__", authorizationTokenCookie["__puc_at__"], {
-            expires: Date.now() + 3600000,
-            domain: authorizationTokenCookie["Domain"],
-            path: authorizationTokenCookie["Path"],
-            sameSite: "lax",
-            httpOnly: true,
-          })
-          .set("__puc_rt__", refreshTokenCookie["puc_rt__"], {
-            expires: Date.now() + 2629800000,
-            domain: refreshTokenCookie["Domain"],
-            path: refreshTokenCookie["Path"],
-            sameSite: "lax",
-            httpOnly: true,
-          });
-
-        return nextResponse;
-      } else {
-        const absoluteURL = new URL("/authenticate", req.nextUrl.origin);
-        return NextResponse.redirect(absoluteURL.toString());
-      }
+      const absoluteURL = new URL("/console", req.nextUrl.origin);
+      return NextResponse.redirect(absoluteURL.toString());
     }
-  } else {
-    const requestHeaders = new Headers(req.headers);
-    requestHeaders.set("x-pathname", req.nextUrl.pathname);
-    return NextResponse.next({
-      request: {
-        headers: requestHeaders,
-      },
-    });
   }
 }
