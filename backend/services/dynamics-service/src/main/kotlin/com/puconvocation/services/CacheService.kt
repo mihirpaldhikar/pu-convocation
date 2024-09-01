d1053cf704edc69f0f26c6ec65ad670893329095
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-09-01 22:27:49 +0530

    fix (dynamics-service): Fix an issue in which SocketException with Broken Pipe Message was thrown due to missing implementation of connection closing.

--- backend/services/dynamics-service/src/main/kotlin/com/puconvocation/services/CacheService.kt
+++ backend/services/dynamics-service/src/main/kotlin/com/puconvocation/services/CacheService.kt
@@ -1,7 +1,21 @@
+/*
+ * Copyright (c) PU Convocation Management System Authors
+ *
+ * This software is owned by PU Convocation Management System Authors.
+ * No part of the software is allowed to be copied or distributed
+ * in any form. Any attempt to do so will be considered a violation
+ * of copyright law.
+ *
+ * This software is protected by copyright law and international
+ * treaties. Unauthorized copying or distribution of this software
+ * is a violation of these laws and could result in severe penalties.
+ */
+
 package com.puconvocation.services
 
 import com.puconvocation.Environment
 import redis.clients.jedis.JedisPool
+
 
 class CacheService(
     environment: Environment,
@@ -10,17 +24,25 @@ class CacheService(
         environment.redisURL
     )
 
-    private val cache = pool.resource
-
     fun set(key: String, value: String) {
-        cache.set(key, value)
+        pool.resource.use { jedis ->
+            jedis.set(key, value)
+        }
+        pool.resource.close()
     }
 
     fun get(key: String): String? {
-        return cache.get(key)
+        val cache = pool.resource.use { jedis ->
+            jedis.get(key)
+        }
+        pool.resource.close()
+        return cache?.toString()
     }
 
     fun remove(key: String) {
-        cache.del(key)
+        pool.resource.use { jedis ->
+            jedis.del(key)
+        }
+        pool.resource.close()
     }
 }
