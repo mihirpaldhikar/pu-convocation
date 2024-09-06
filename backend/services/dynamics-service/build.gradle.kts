4b086ce39e941614bce3e4842062caff4bce2b36
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-09-07 01:19:33 +0530

    deps (dynamics-service): Remove unused dependencies.

--- backend/services/dynamics-service/build.gradle.kts
+++ backend/services/dynamics-service/build.gradle.kts
@@ -46,12 +46,10 @@ dependencies {
     implementation("io.ktor:ktor-server-cors-jvm")
     implementation("io.ktor:ktor-server-call-logging-jvm")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
-    implementation("io.ktor:ktor-serialization-gson-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("io.ktor:ktor-client-core")
     implementation("io.ktor:ktor-client-cio")
     implementation("io.ktor:ktor-client-content-negotiation")
-    implementation("io.ktor:ktor-serialization-gson")
     implementation("io.ktor:ktor-serialization-jackson-jvm")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
 
