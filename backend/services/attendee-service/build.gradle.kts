6888d10c83db31f82b7bde53515ffadba486ade9
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-09-11 19:30:38 +0530

    build (attendee-service): Upgrade the Kotlin version to 2.0.20

--- backend/services/attendee-service/build.gradle.kts
+++ backend/services/attendee-service/build.gradle.kts
@@ -19,8 +19,8 @@ val mongoDBVersion: String by project
 val apacheCSV: String by project
 
 plugins {
-    kotlin("jvm") version "1.9.24"
-    id("io.ktor.plugin") version "2.3.10"
+    kotlin("jvm") version "2.0.20"
+    id("io.ktor.plugin") version "2.3.12"
 }
 
 group = "com.puconvocation"
