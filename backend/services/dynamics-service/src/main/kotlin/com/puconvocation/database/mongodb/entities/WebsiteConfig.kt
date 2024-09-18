64780a2f143285534b0082014e974b44ce7ca3d8
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-09-18 22:55:29 +0530

    refactor (dynamics-service): Refactor WebsiteConfig inorder to comply with the latest changes to Enclosure Mappings.

--- backend/services/dynamics-service/src/main/kotlin/com/puconvocation/database/mongodb/entities/WebsiteConfig.kt
+++ backend/services/dynamics-service/src/main/kotlin/com/puconvocation/database/mongodb/entities/WebsiteConfig.kt
@@ -1,6 +1,20 @@
+/*
+ * Copyright (c) PU Convocation Management System Authors
+ *
+ * This software is owned by PU Convocation Management System Authors.
+ * No part of the software is allowed to be copied or distributed
+ * in any form. Any attempt to do so will be considered a violation
+ * of copyright law.
+ *
+ * This software is protected by copyright law and international
+ * treaties. Unauthorized copying or distribution of this software
+ * is a violation of these laws and could result in severe penalties.
+ */
+
 package com.puconvocation.database.mongodb.entities
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.puconvocation.enums.Direction
 import org.bson.codecs.pojo.annotations.BsonId
 
 data class WebsiteConfig(
@@ -24,6 +38,7 @@ data class WebsiteConfig(
 
     data class Enclosure(
         @JsonProperty("letter") val letter: String,
+        @JsonProperty("entryDirection") val entryDirection: Direction,
         @JsonProperty("rows") val rows: MutableList<Row>,
     ) {
         data class Row(
