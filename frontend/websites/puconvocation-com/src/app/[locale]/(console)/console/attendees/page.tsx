3beb423e706881da4e65e0496a710532b486f7cd
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-09-23 14:21:08 +0530

    chore (puconvocation-com): Add Pages with respect to navigation menu routes.

---
+++ frontend/websites/puconvocation-com/src/app/[locale]/(console)/console/attendees/page.tsx
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) PU Convocation Management System Authors
+ *
+ * This software is owned by PU Convocation Management System Authors.
+ * No part of the software is allowed to be copied or distributed
+ * in any form. Any attempt to do so will be considered a violation
+ * of copyright law.
+ *
+ * This software is protected by copyright law and international
+ * treaties. Unauthorized copying or distribution of this software
+ * is a violation of these laws and could result in severe penalties.
+ */
+
+import { JSX } from "react";
+
+export default function AttendeePage(): JSX.Element {
+  return (
+    <div className={"flex min-h-screen"}>
+      <div className={"m-auto"}>
+        <div>
+          <h1 className={"text-2xl font-bold"}>Attendees</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
