3f70721ff90b679336e606ba0b687f9d086652b9
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-09-23 16:32:59 +0530

    refactor (puconvocation-com): Refactor QR Code Scanner page to use Sheets inorder to show attendee details instead of redirecting to a new page for verification.

--- frontend/websites/puconvocation-com/src/app/[locale]/(console)/console/scan/page.tsx
+++ frontend/websites/puconvocation-com/src/app/[locale]/(console)/console/scan/page.tsx
@@ -11,15 +11,42 @@
  * is a violation of these laws and could result in severe penalties.
  */
 "use client";
-import { JSX } from "react";
+import { Fragment, JSX, useState } from "react";
 import { useZxing } from "react-zxing";
 import { QrCodeIcon } from "@heroicons/react/24/outline";
-import { useRouter } from "next/navigation";
 import { useTranslations } from "use-intl";
+import {
+  Button,
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@components/ui";
+import { AttendeeService } from "@services/index";
+import { DynamicIcon, ProgressBar } from "@components/index";
+import { CheckBadgeIcon } from "@heroicons/react/24/solid";
+import { StatusCode } from "@enums/StatusCode";
+import { Attendee } from "@dto/index";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@components/ui/input_otp";
+import { useToast } from "@hooks/useToast";
+
+const attendeeService = new AttendeeService();
 
 export default function VerificationPage(): JSX.Element {
-  const router = useRouter();
   const pageTranslations = useTranslations("pages.scanPage");
+
+  const { toast } = useToast();
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [attendee, setAttendee] = useState<Attendee | null>(null);
+  const [verificationCode, setVerificationCode] = useState<string>("");
 
   const { ref: qrCodeScanner } = useZxing({
     onDecodeResult(result) {
@@ -27,25 +54,168 @@ export default function VerificationPage(): JSX.Element {
       const verificationToken = result.getText();
 
       if (tokenRegex.test(verificationToken)) {
-        router.push(`/console/verify?token=${verificationToken}`);
+        setOpen(true);
+        attendeeService
+          .getAttendeeFromVerificationToken(verificationToken)
+          .then((result) => {
+            if (
+              result.statusCode === StatusCode.SUCCESS &&
+              "payload" in result &&
+              typeof result.payload === "object"
+            ) {
+              setAttendee(result.payload);
+            }
+            setLoading(false);
+          });
       }
     },
   });
 
   return (
-    <section className={"flex min-h-dvh"}>
-      <div className="m-auto flex w-full flex-col items-center justify-center">
-        <div className={"flex flex-col items-center justify-center space-y-3"}>
-          <QrCodeIcon className={"size-14 text-red-600"} />
-          <h6 className={"text-xl font-semibold"}>
-            {pageTranslations("title")}
-          </h6>
+    <Fragment>
+      <section className={"flex min-h-dvh"}>
+        <div className="m-auto flex w-full flex-col items-center justify-center">
+          <div
+            className={"flex flex-col items-center justify-center space-y-3"}
+          >
+            <QrCodeIcon className={"size-14 text-red-600"} />
+            <h6 className={"text-xl font-semibold"}>
+              {pageTranslations("title")}
+            </h6>
+          </div>
+          <div className={"p-10"}>
+            <video
+              ref={qrCodeScanner}
+              style={{
+                borderRadius: "1rem",
+              }}
+            />
+          </div>
         </div>
-        <video ref={qrCodeScanner} className={"h-96 w-96 rounded-lg"} />
-        <p className={"text-center text-lg"}>
-          {pageTranslations("description")}
-        </p>
-      </div>
-    </section>
+      </section>
+      <Sheet open={open} onOpenChange={(currentState) => setOpen(currentState)}>
+        <SheetContent side={"bottom"} className={"h-2/3"}>
+          <SheetTitle />
+          <SheetHeader />
+          <SheetDescription />
+          <div className={"h-full"}>
+            {loading ? (
+              <div className={"flex h-full w-full items-center justify-center"}>
+                <ProgressBar type={"circular"} />
+              </div>
+            ) : !loading && attendee === null ? (
+              <div
+                className={
+                  "flex h-full w-full flex-col items-center justify-center space-y-5"
+                }
+              >
+                <DynamicIcon
+                  icon={"XCircleIcon"}
+                  outline={false}
+                  className={"size-14 text-red-600"}
+                />
+                <h5 className={"text-lg font-semibold"}>Attendee Not Found!</h5>
+              </div>
+            ) : (
+              attendee !== null && (
+                <Fragment>
+                  <div
+                    className={
+                      "flex flex-col items-center justify-center space-y-3"
+                    }
+                  >
+                    <CheckBadgeIcon className={"size-14 text-green-600"} />
+                    <h4 className={"text-xl font-bold"}>Confirmation</h4>
+                  </div>
+                  <div>
+                    <div className={"space-y-3"}>
+                      <h4 className={"text-lg font-semibold"}>
+                        Student Name: {attendee.studentName}
+                      </h4>
+                      <h4 className={"text-lg font-semibold"}>
+                        Enrollment No: {attendee.enrollmentNumber}
+                      </h4>
+                      <h4 className={"text-lg font-semibold"}>
+                        Department: {attendee.department}
+                      </h4>
+                      <h4 className={"text-lg font-semibold"}>
+                        Institute: {attendee.institute}
+                      </h4>
+                    </div>
+                    <div
+                      className={
+                        "flex w-full flex-col items-center justify-center space-y-3 py-5"
+                      }
+                    >
+                      <h5>Enter Verification Code</h5>
+                      <InputOTP
+                        maxLength={6}
+                        onChange={(code) => {
+                          setVerificationCode(code);
+                        }}
+                      >
+                        <InputOTPGroup>
+                          <InputOTPSlot index={0} />
+                          <InputOTPSlot index={1} />
+                          <InputOTPSlot index={2} />
+                        </InputOTPGroup>
+                        <InputOTPSeparator />
+                        <InputOTPGroup>
+                          <InputOTPSlot index={3} />
+                          <InputOTPSlot index={4} />
+                          <InputOTPSlot index={5} />
+                        </InputOTPGroup>
+                      </InputOTP>
+                    </div>
+                    <SheetClose asChild={true}>
+                      <Button
+                        disabled={verificationCode.length !== 6}
+                        className={"w-full"}
+                        onClick={async () => {
+                          if (verificationCode === attendee.verificationCode) {
+                            setLoading(true);
+                            setVerificationCode("");
+                            const response =
+                              await attendeeService.createTransaction(
+                                attendee.enrollmentNumber,
+                              );
+
+                            if (
+                              response.statusCode === StatusCode.FAILURE &&
+                              "message" in response
+                            ) {
+                              toast({
+                                title: "Transaction Failed",
+                                description: response.message,
+                                duration: 5000,
+                              });
+                            } else {
+                              toast({
+                                title: "Success",
+                                description:
+                                  "Transaction completed successfully.",
+                                duration: 5000,
+                              });
+                            }
+                          } else {
+                            toast({
+                              title: "Transaction Failed",
+                              description: "Invalid Verification Code.",
+                              duration: 5000,
+                            });
+                          }
+                        }}
+                      >
+                        Submit
+                      </Button>
+                    </SheetClose>
+                  </div>
+                </Fragment>
+              )
+            )}
+          </div>
+        </SheetContent>
+      </Sheet>
+    </Fragment>
   );
 }
--- frontend/websites/puconvocation-com/src/services/AttendeeService.ts
+++ frontend/websites/puconvocation-com/src/services/AttendeeService.ts
@@ -70,7 +70,7 @@ export default class AuthService {
         studentEnrollmentNumber: studentEnrollmentNumber,
       },
       {
-        expectedStatusCode: 201,
+        expectedStatusCode: 200,
       },
     );
   }
