7dc8ac23478ae1a7704d01067d84d32b292e5118
Suhani-013 <126856854+Suhani-013@users.noreply.github.com>
2024-10-04 12:16:56 +0530

    feat(puconvocation-com): Implement attendee search and dynamic pagination calculation

--- frontend/websites/puconvocation-com/src/app/[locale]/(console)/console/attendees/page.tsx
+++ frontend/websites/puconvocation-com/src/app/[locale]/(console)/console/attendees/page.tsx
@@ -12,7 +12,7 @@
  */
 "use client";
 
-import { JSX, useState } from "react";
+import { JSX, useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { Button } from "@components/ui";
 import {
@@ -28,6 +28,7 @@ import {
   ChevronLeftIcon,
   ChevronRightIcon,
   UsersIcon,
+  MagnifyingGlassIcon,
 } from "@heroicons/react/24/solid";
 import { ProgressBar } from "@components/index";
 
@@ -37,22 +38,38 @@ export default function AttendeePage(): JSX.Element {
   const [showAttendees, setShowAttendees] = useState(false);
   const [page, setPage] = useState(0);
   const [attendeeCount, setAttendeeCount] = useState(0);
+  const [totalAttendeeCount, setTotalAttendeeCount] = useState(0); // Total attendees from DB
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const fetchTotalAttendeeCount = async () => {
+      const response = await attendeeController.getTotalAttendeesCount();
+      if (response.statusCode === StatusCode.SUCCESS) {
+        setTotalAttendeeCount(response.payload as number); // Set total count
+      }
+    };
+
+    fetchTotalAttendeeCount();
+  }, []);
 
   const {
     data: attendees,
     isLoading: isAttendeeLoading,
     isError: attendeeError,
   } = useQuery({
-    queryKey: ["homeAttendeesList", page],
+    queryKey: ["attendeesList", page, searchTerm],
     refetchOnWindowFocus: false,
     queryFn: async () => {
-      const response = await attendeeController.getAllAttendees(page, 10);
+      const response = await attendeeController.getAllAttendees(
+        page,
+        10,
+        searchTerm,
+      );
       if (
         response.statusCode === StatusCode.SUCCESS &&
         "payload" in response &&
         typeof response.payload === "object"
       ) {
-        setAttendeeCount(response.payload.totalCount);
         return response.payload.attendees;
       }
       return [];
@@ -60,21 +77,21 @@ export default function AttendeePage(): JSX.Element {
   });
 
   const currentAttendees = attendees || [];
+  const totalPages = Math.ceil(totalAttendeeCount / 10);
 
   return (
-    <div className={"flex min-h-screen flex-col space-y-10 p-4 md:p-10"}>
-      <div className={"space-y-3"}>
-        <h1 className={"flex items-center text-2xl font-bold"}>
+    <div className="flex min-h-screen flex-col space-y-10 p-4 md:p-10">
+      <div className="space-y-3">
+        <h1 className="flex items-center text-2xl font-bold">
           <UsersIcon className="mr-2 h-6 w-6 text-red-600" /> Attendees
         </h1>
-        <p className={"text-xs text-gray-600"}>
+        <p className="text-xs text-gray-600">
           View and manage the complete list of attendees for the convocation.
         </p>
       </div>
 
-      <div className={"flex min-h-screen flex-col items-center p-1"}>
+      <div className="flex min-h-screen flex-col items-center p-1">
         <div className="mb-2 grid w-full grid-cols-1 gap-6 lg:grid-cols-1">
-          {/* Total Attendees */}
           <Card className="h-fit w-full border border-gray-300 p-4">
             <CardHeader>
               <CardTitle>Total Attendees</CardTitle>
@@ -83,7 +100,9 @@ export default function AttendeePage(): JSX.Element {
               </CardDescription>
             </CardHeader>
             <CardContent className="flex items-center justify-between">
-              <span className="text-lg">{attendeeCount}</span>
+              <span className="text-5xl font-bold text-red-600">
+                {totalAttendeeCount}
+              </span>{" "}
               <Button
                 onClick={() => setShowAttendees(!showAttendees)}
                 className="bg-red-600 text-white hover:bg-red-700"
@@ -93,8 +112,7 @@ export default function AttendeePage(): JSX.Element {
             </CardContent>
           </Card>
 
-          {/* Attendee List */}
-          <Card className="h-fit w-full border border-gray-300 p-4 shadow-none">
+          <Card className="h-full w-full flex-grow border border-gray-300 p-4 shadow-none">
             <CardHeader>
               <CardTitle>Attendee List</CardTitle>
               <CardDescription>
@@ -102,7 +120,18 @@ export default function AttendeePage(): JSX.Element {
                 respective information.
               </CardDescription>
             </CardHeader>
-            <CardContent className="flex h-96 flex-col overflow-y-auto">
+            <CardContent className="flex h-full flex-col">
+              <div className="relative mb-4">
+                <MagnifyingGlassIcon className="absolute left-3 h-10 w-5 text-gray-500" />
+                <input
+                  type="text"
+                  placeholder="Search Attendees..."
+                  className="w-1/4 rounded-lg bg-gray-100 p-2 pl-10"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+              </div>
+
               {isAttendeeLoading ? (
                 <div className="flex h-full items-center justify-center">
                   <ProgressBar type="circular" />
@@ -110,67 +139,70 @@ export default function AttendeePage(): JSX.Element {
               ) : attendeeError ? (
                 <p className="text-red-600">Error loading attendees</p>
               ) : currentAttendees.length > 0 ? (
-                <table className="min-w-full table-auto border-collapse">
-                  <thead>
-                    <tr>
-                      <th className="px-4 py-2 text-center font-semibold text-gray-700">
-                        Convocation Id
-                      </th>
-                      <th className="px-4 py-2 text-center font-semibold text-gray-700">
-                        Name
-                      </th>
-                      <th className="px-4 py-2 text-center font-semibold text-gray-700">
-                        Enclosure
-                      </th>
-                      <th className="px-4 py-2 text-center font-semibold text-gray-700">
-                        Row
-                      </th>
-                      <th className="px-4 py-2 text-center font-semibold text-gray-700">
-                        Seat
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {currentAttendees.map((a) => (
-                      <tr
-                        key={a.convocationId}
-                        className="border-b text-center transition-colors duration-200 hover:bg-gray-100"
-                      >
-                        <td className="px-4 py-2">{a.convocationId}</td>
-                        <td className="px-4 py-2">{a.studentName}</td>
-                        <td className="px-4 py-2">{a.allocation.enclosure}</td>
-                        <td className="px-4 py-2">{a.allocation.row}</td>
-                        <td className="px-4 py-2">{a.allocation.seat}</td>
+                <div className="flex-grow overflow-y-auto">
+                  <table className="min-w-full table-auto border-collapse">
+                    <thead>
+                      <tr>
+                        <th className="px-4 py-2 text-center font-semibold text-gray-700">
+                          Convocation Id
+                        </th>
+                        <th className="px-4 py-2 text-center font-semibold text-gray-700">
+                          Name
+                        </th>
+                        <th className="px-4 py-2 text-center font-semibold text-gray-700">
+                          Enclosure
+                        </th>
+                        <th className="px-4 py-2 text-center font-semibold text-gray-700">
+                          Row
+                        </th>
+                        <th className="px-4 py-2 text-center font-semibold text-gray-700">
+                          Seat
+                        </th>
                       </tr>
-                    ))}
-                  </tbody>
-                </table>
+                    </thead>
+                    <tbody>
+                      {currentAttendees.map((a) => (
+                        <tr
+                          key={a.convocationId}
+                          className="border-b text-center transition-colors duration-200 hover:bg-gray-100"
+                        >
+                          <td className="px-4 py-2">{a.convocationId}</td>
+                          <td className="px-4 py-2">{a.studentName}</td>
+                          <td className="px-4 py-2">
+                            {a.allocation.enclosure}
+                          </td>
+                          <td className="px-4 py-2">{a.allocation.row}</td>
+                          <td className="px-4 py-2">{a.allocation.seat}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+
+                  <div className="mt-4 flex items-center justify-end">
+                    <Button
+                      onClick={() => setPage((prev) => Math.max(prev - 1, 0))}
+                      disabled={page === 0}
+                      className="flex items-center justify-center rounded border border-gray-300 bg-white p-2 hover:bg-gray-300"
+                    >
+                      <ChevronLeftIcon className="h-6 w-6 text-black" />
+                    </Button>
+                    <span className="mx-2 text-lg">
+                      {page + 1}/{totalPages}
+                    </span>
+                    <Button
+                      onClick={() => setPage((prev) => prev + 1)}
+                      disabled={currentAttendees.length < 10}
+                      className="flex items-center justify-center rounded border border-gray-300 bg-white p-2 hover:bg-gray-300"
+                    >
+                      <ChevronRightIcon className="h-6 w-6 text-black" />
+                    </Button>
+                  </div>
+                </div>
               ) : (
                 <p>No attendees found</p>
               )}
             </CardContent>
           </Card>
-        </div>
-
-        {/* Pagination Controls */}
-        <div className="mt-4 flex w-full justify-end">
-          <div className="flex items-center space-x-2">
-            <Button
-              onClick={() => setPage((prev) => Math.max(prev - 1, 0))}
-              disabled={page === 0}
-              className="flex h-12 w-12 items-center justify-center rounded-full border border-gray-300 bg-white hover:bg-gray-300"
-            >
-              <ChevronLeftIcon className="h-8 w-8 text-black" />
-            </Button>
-            <span className="text-lg">Page {page + 1}</span>
-            <Button
-              onClick={() => setPage((prev) => prev + 1)}
-              disabled={currentAttendees.length < 10}
-              className="flex h-12 w-12 items-center justify-center rounded-full border border-gray-300 bg-white hover:bg-gray-300"
-            >
-              <ChevronRightIcon className="h-8 w-8 text-black" />
-            </Button>
-          </div>
         </div>
       </div>
     </div>
