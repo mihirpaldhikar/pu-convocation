d0e9e57d8e50128f9951059b5833bf67118fb871
Suhani-013 <126856854+Suhani-013@users.noreply.github.com>
2024-10-05 18:31:02 +0530

    feat (puconvocation-com): Add layout for settings in console

---
+++ frontend/websites/puconvocation-com/src/app/[locale]/(console)/console/settings/layout.tsx
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) PU Convocation Management System Authors
+ *
+ * This software is owned by PU Convocation Management System Authors.
+ * No part of the software is allowed to be copied or distributed
+ * in any form. Any attempt to do so will be considered a violation
+ * of copyright law.
+ *
+ * This software is protected by copyright law and international
+ * treaties. Unauthorized copying or distribution of this software
+ * is a violation of these laws and could result in severe penalties.
+ */
+"use client";
+import { JSX, ReactNode } from "react";
+import { Link, usePathname } from "@i18n/routing";
+import { Cog6ToothIcon } from "@heroicons/react/24/solid";
+
+
+interface SettingsLayoutProps {
+  children: ReactNode;
+}
+
+const tabs: Array<{
+  name: string;
+  route: string;
+}> = [
+  {
+    name: "General",
+    route: "",
+  },
+  {
+    name: "Ground",
+    route: "/ground",
+  },
+  {
+    name: "Instructions",
+    route: "/instructions",
+  },
+];
+
+export default function SettingsLayout({
+  children,
+}: SettingsLayoutProps): JSX.Element {
+  const path = usePathname();
+
+  return (
+    <div className="flex min-h-screen flex-col space-y-10 p-4 md:p-10">
+      <div className="space-y-3">
+        <h1 className="flex items-center text-2xl font-bold">
+          <Cog6ToothIcon className="mr-2 h-6 w-6 fill-red-600" /> Settings
+        </h1>
+        <p className="text-xs text-gray-600">
+          Manage your preferences and configure your account settings here.
+        </p>
+      </div>
+    <div>
+      <div
+        className={
+          "flex w-full items-center justify-evenly rounded-xl border bg-white p-1 space-x-4"
+        }
+      >
+        {tabs.map((tab) => {
+          return (
+            <Link
+              key={tab.name}
+              href={`/console/settings${tab.route}`}
+              className={`${path === `/console/settings${tab.route}` ? "bg-red-100 text-red-800" : "text-gray-500 hover:bg-gray-200"} transition-all duration-200 w-full rounded-lg flex items-center justify-center p-1 font-semibold`}
+            >
+              {tab.name}
+            </Link>
+          );
+        })}
+      </div>
+      <div className={"pt-5"}>{children}</div>
+    </div>
+    </div>
+  );
+}
