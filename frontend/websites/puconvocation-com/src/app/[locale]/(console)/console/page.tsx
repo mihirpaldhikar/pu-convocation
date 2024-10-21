37377b12f167d7ec40c5589fb4ce5993520e32df
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-10-21 16:38:13 +0530

    fix (puconvocation-com): Fix missing IAM Checks on Console Home Page.

--- frontend/websites/puconvocation-com/src/app/[locale]/(console)/console/page.tsx
+++ frontend/websites/puconvocation-com/src/app/[locale]/(console)/console/page.tsx
@@ -11,7 +11,7 @@
  * is a violation of these laws and could result in severe penalties.
  */
 
-import { JSX } from "react";
+import { Fragment, JSX } from "react";
 import { Link } from "@i18n/routing";
 import { Button, Card, CardContent, CardHeader, CardTitle } from "@components/ui";
 import { PopularCountriesChart, TrafficOnDateChart } from "@components/charts";
@@ -83,57 +83,71 @@ export default async function ConsolePage(): Promise<JSX.Element> {
   return (
     <div className="bg-white-300 flex min-h-screen flex-col space-y-10 p-4 md:p-10 lg:p-20">
       {/* Analytics Section */}
-      <div
-        hidden={
-          account === null || !account?.iamRoles.includes("read:Analytics")
-        }
-      >
-        <div className="mb-6 flex items-center justify-between">
-          <h2 className="text-xl font-bold">Analytics</h2>
-          <Button className="bg-red-600 font-semibold hover:bg-red-700" asChild>
-            <Link href={`/console/analytics`}>View All</Link>
-          </Button>
-        </div>
+      {account === null || !account?.iamRoles.includes("read:Analytics") ? (
+        <Fragment />
+      ) : (
+        <section>
+          <div className="mb-6 flex items-center justify-between">
+            <h2 className="text-xl font-bold">Analytics</h2>
+            <Button
+              className="bg-red-600 font-semibold hover:bg-red-700"
+              asChild={true}
+            >
+              <Link href={`/console/analytics`}>View All</Link>
+            </Button>
+          </div>
 
-        <div className="cards mb-4 grid grid-cols-1 gap-6 lg:grid-cols-2">
-          {/* Demographics Card */}
-          <Card className="h-full overflow-hidden p-2 shadow-none">
-            <CardHeader className="pb-2">
-              <CardTitle className="text-red-600">Demographics</CardTitle>
-            </CardHeader>
-            <CardContent className="h-full py-5">
-              <PopularCountriesChart
-                analytics={popularCountries}
-                showLegends={false}
-              />
-            </CardContent>
-          </Card>
+          <div className="cards mb-4 grid grid-cols-1 gap-6 lg:grid-cols-2">
+            {/* Demographics Card */}
+            <Card className="h-full overflow-hidden p-2 shadow-none">
+              <CardHeader className="pb-2">
+                <CardTitle className="text-red-600">Demographics</CardTitle>
+              </CardHeader>
+              <CardContent className="h-full py-5">
+                <PopularCountriesChart
+                  analytics={popularCountries}
+                  showLegends={false}
+                />
+              </CardContent>
+            </Card>
 
-          {/* Traffic Card */}
-          <Card className="h-fit overflow-hidden p-2 shadow-none">
-            <CardHeader className="pb-2">
-              <CardTitle className="text-red-600">Traffic</CardTitle>
-            </CardHeader>
-            <CardContent className="h-full pt-2">
-              <TrafficOnDateChart analytics={dailyVisitors} />
-            </CardContent>
-          </Card>
-        </div>
-      </div>
+            {/* Traffic Card */}
+            <Card className="h-fit overflow-hidden p-2 shadow-none">
+              <CardHeader className="pb-2">
+                <CardTitle className="text-red-600">Traffic</CardTitle>
+              </CardHeader>
+              <CardContent className="h-full pt-2">
+                <TrafficOnDateChart analytics={dailyVisitors} />
+              </CardContent>
+            </Card>
+          </div>
+        </section>
+      )}
 
       {/* Attendees Section */}
-      <div className="mb-6 flex items-center justify-between">
-        <h2 className="text-xl font-bold">Attendees</h2>
-        <Button className="bg-red-600 font-semibold hover:bg-red-700" asChild>
-          <Link href={`/console/attendees`}>View All</Link>
-        </Button>
-      </div>
-      <Card>
-        <CardHeader></CardHeader>
-        <CardContent>
-          <AttendeeTable initialAttendees={attendees} />
-        </CardContent>
-      </Card>
+      {account === null ||
+      (!account?.iamRoles.includes("write:Attendee") &&
+        !account?.iamRoles.includes("read:Attendee")) ? (
+        <Fragment />
+      ) : (
+        <section>
+          <div className="mb-6 flex items-center justify-between">
+            <h2 className="text-xl font-bold">Attendees</h2>
+            <Button
+              className="bg-red-600 font-semibold hover:bg-red-700"
+              asChild
+            >
+              <Link href={`/console/attendees`}>View All</Link>
+            </Button>
+          </div>
+          <Card>
+            <CardHeader></CardHeader>
+            <CardContent>
+              <AttendeeTable initialAttendees={attendees} />
+            </CardContent>
+          </Card>
+        </section>
+      )}
     </div>
   );
 }
