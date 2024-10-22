f1b0c6cb4dff8dc949b82f3838405ec5c9e37675
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-10-23 00:08:33 +0530

    style (puconvocation-com): Upgrade ESLint to v9.13.0 as ESLint v8.x has reached end-of-life on 2024-10-05.

---
+++ frontend/websites/puconvocation-com/eslint.config.mjs
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) PU Convocation Management System Authors
+ *
+ * This software is owned by PU Convocation Management System Authors.
+ * No part of the software is allowed to be copied or distributed
+ * in any form. Any attempt to do so will be considered a violation
+ * of copyright law.
+ *
+ * This software is protected by copyright law and international
+ * treaties. Unauthorized copying or distribution of this software
+ * is a violation of these laws and could result in severe penalties.
+ */
+
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+const modules = [...compat.extends("next/core-web-vitals", "next", "prettier")];
+export default modules;
