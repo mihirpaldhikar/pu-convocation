20bc742b29194da7b5a7dd937bee9831d06fee3a
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-10-23 21:01:36 +0530

    fix (puconvocation-com): Fix an issue in which the Instructions were not rendering on the instructions page due to conflict between RemoteMarkdown and React 19.

--- frontend/websites/puconvocation-com/src/app/[locale]/(default)/instructions/page.tsx
+++ frontend/websites/puconvocation-com/src/app/[locale]/(default)/instructions/page.tsx
@@ -11,9 +11,7 @@
  * is a violation of these laws and could result in severe penalties.
  */
 
-import { MDXRemote } from "next-mdx-remote/rsc";
-import Image from "next/image";
-import { Link } from "@i18n/routing";
+import Markdown from "react-markdown";
 
 export default async function InstructionsPage() {
   const response = await fetch(
@@ -21,77 +19,9 @@ export default async function InstructionsPage() {
   );
   return (
     <div className={"min-h-screen w-full p-5 lg:p-10"}>
-      <MDXRemote
-        source={await response.text()}
-        components={{
-          h1: (props) => {
-            return (
-              <h1
-                className={"py-5 text-2xl font-black text-red-600 lg:text-3xl"}
-              >
-                {props.children}
-              </h1>
-            );
-          },
-          h2: (props) => {
-            return (
-              <h2 className={"py-3 text-lg font-bold lg:text-xl text-red-600"}>
-                {props.children}
-              </h2>
-            );
-          },
-          h3: (props) => {
-            return <h3 className={"py-2 font-semibold text-red-600"}>{props.children}</h3>;
-          },
-          ul: (props) => {
-            return (
-              <ul
-                className={
-                  "list-inside list-disc space-y-2 pl-5 marker:text-red-600"
-                }
-              >
-                {props.children}
-              </ul>
-            );
-          },
-          ol: (props) => {
-            return (
-              <ol
-                className={
-                  "list-inside list-decimal space-y-2 pl-5 marker:text-red-600"
-                }
-              >
-                {props.children}
-              </ol>
-            );
-          },
-          img: (props) => {
-            return (
-              <Image
-                src={props.src ?? ""}
-                alt={props.alt ?? ""}
-                className={"w-full rounded-md"}
-                width={500}
-                height={300}
-              />
-            );
-          },
-          p: (props) => {
-            return <p className={"py-3 text-gray-800"}>{props.children}</p>;
-          },
-          a: (props) => {
-            return (
-              <Link
-                href={props.href ?? ""}
-                className={"text-red-800 underline"}
-                target={"_blank"}
-              >
-                {props.children}
-              </Link>
-            );
-          },
-        }}
-      />
+      <Markdown className={"prose prose-h1:text-red-800"}>
+        {await response.text()}
+      </Markdown>
     </div>
   );
 }
