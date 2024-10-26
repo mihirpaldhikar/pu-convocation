cd64d69286de676846d9d3ca2e32b48480c3f145
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-10-26 22:24:05 +0530

    deps (auth-service): Upgrade dependencies to the latest versions.

--- backend/services/auth-service/build.gradle.kts
+++ backend/services/auth-service/build.gradle.kts
@@ -19,7 +19,7 @@ val webauthnVersion: String by project
 
 plugins {
     kotlin("jvm") version "2.0.20"
-    id("io.ktor.plugin") version "3.0.0-rc-1"
+    id("io.ktor.plugin") version "3.0.0"
 }
 
 group = "com.puconvocation"
