8409af85c4a18afd005e51aa20f645126e39b607
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-10-27 22:37:39 +0530

    fix (puconvocation-com): Fix ESLint issues in useToast.ts

--- frontend/websites/puconvocation-com/src/hooks/useToast.ts
+++ frontend/websites/puconvocation-com/src/hooks/useToast.ts
@@ -15,7 +15,7 @@
 
 import * as React from "react";
 
-import type {ToastActionElement, ToastProps} from "@components/ui/toast";
+import type { ToastActionElement, ToastProps } from "@components/ui/toast";
 
 const TOAST_LIMIT = 1;
 const TOAST_REMOVE_DELAY = 1000000;
@@ -27,7 +27,6 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const actionTypes = {
   ADD_TOAST: "ADD_TOAST",
   UPDATE_TOAST: "UPDATE_TOAST",
