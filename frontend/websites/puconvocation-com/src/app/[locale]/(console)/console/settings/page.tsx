b17552bed1a8f278d361c8130b20fef66b919530
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-11-18 17:13:03 +0530

    feat (puconvocation-com): Add the ability to upload images in the Image Picker dialog.

--- frontend/websites/puconvocation-com/src/app/[locale]/(console)/console/settings/page.tsx
+++ frontend/websites/puconvocation-com/src/app/[locale]/(console)/console/settings/page.tsx
@@ -11,7 +11,7 @@
  * is a violation of these laws and could result in severe penalties.
  */
 "use client";
-import { Fragment, JSX } from "react";
+import { Fragment, JSX, useState } from "react";
 import { useRemoteConfig } from "@hooks/index";
 import Image from "next/image";
 import { DynamicIcon } from "@components/graphics";
@@ -22,7 +22,7 @@ import {
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger
+  FilePicker,
 } from "@components/ui";
 import { useQuery } from "@tanstack/react-query";
 import { StatusCode } from "@enums/StatusCode";
@@ -33,11 +33,12 @@ const assetsController = new AssetsController();
 
 export default function GeneralSettingsPage(): JSX.Element {
   const { remoteConfig, dispatch } = useRemoteConfig();
+  const [showImagePicker, setShowImagePicker] = useState(false);
 
   const {
     data: imageLibrary,
     isLoading: imageLibraryLoading,
-    refetch: fetchImageLibrary
+    refetch: fetchImageLibrary,
   } = useQuery({
     queryKey: ["imageLibrary"],
     enabled: false,
@@ -51,11 +52,100 @@ export default function GeneralSettingsPage(): JSX.Element {
         return response.payload;
       }
       return null;
-    }
+    },
   });
 
   return (
     <div className={"min-h-screen w-full rounded-xl border bg-white px-4 py-5"}>
+      <Dialog
+        open={showImagePicker}
+        onOpenChange={(open) => setShowImagePicker(open)}
+      >
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle asChild={true}>
+              <div className={"space-y-3"}>
+                <div className={"flex w-full justify-between"}>
+                  <h1>Image Library</h1>
+                  <div className={"relative flex-1 pr-10"}>
+                    <div
+                      className={
+                        "absolute right-10 top-[-1rem] rounded-2xl bg-black px-3 py-2 text-xs text-white"
+                      }
+                    >
+                      Upload
+                    </div>
+                    <FilePicker
+                      allowedFileExtensions={".avif"}
+                      onFilePicked={async (file) => {
+                        if (file !== null) {
+                          const response =
+                            await assetsController.uploadImage(file);
+                          if (response.statusCode === StatusCode.SUCCESS) {
+                            await fetchImageLibrary();
+                          }
+                        }
+                      }}
+                    />
+                  </div>
+                </div>
+                <p className={"text-xs text-gray-400"}>
+                  Click on the image to add it.
+                </p>
+              </div>
+            </DialogTitle>
+            <DialogDescription
+              asChild={true}
+              className={"max-h-72 w-full overflow-y-auto pt-10"}
+            >
+              {imageLibraryLoading ? (
+                <Fragment>Loading...</Fragment>
+              ) : imageLibrary !== null && imageLibrary !== undefined ? (
+                <div className={"grid w-full grid-cols-3 gap-4"}>
+                  {imageLibrary.map((image) => {
+                    return (
+                      <DialogClose
+                        key={image}
+                        onClick={() => {
+                          dispatch({
+                            type: "SET_CONFIG",
+                            payload: {
+                              config: {
+                                ...remoteConfig,
+                                images: {
+                                  ...remoteConfig.images,
+                                  carousel: [
+                                    ...remoteConfig.images.carousel,
+                                    {
+                                      url: image,
+                                      description: "Image from Library",
+                                    },
+                                  ],
+                                },
+                              },
+                            },
+                          });
+                        }}
+                      >
+                        <Image
+                          src={image}
+                          alt={"Image"}
+                          width={200}
+                          height={100}
+                          className={"h-full w-full rounded-xl"}
+                        />
+                      </DialogClose>
+                    );
+                  })}
+                </div>
+              ) : (
+                <Fragment>Cannot load Images</Fragment>
+              )}
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+
       <section className={"space-y-5"}>
         <h3 className={"text-2xl font-bold"}>Images</h3>
         <div className={"space-y-5"}>
@@ -65,7 +155,10 @@ export default function GeneralSettingsPage(): JSX.Element {
           >
             {remoteConfig.images.carousel.map((image, index) => {
               return (
-                <div key={image.url} className={"relative"}>
+                <div
+                  key={image.url.concat(Math.random().toString())}
+                  className={"relative"}
+                >
                   <Image
                     src={image.url}
                     alt={image.description}
@@ -90,10 +183,10 @@ export default function GeneralSettingsPage(): JSX.Element {
                               ...remoteConfig,
                               images: {
                                 ...remoteConfig.images,
-                                carousel: [...x]
-                              }
-                            }
-                          }
+                                carousel: [...x],
+                              },
+                            },
+                          },
                         });
                       }}
                     >
@@ -104,83 +197,19 @@ export default function GeneralSettingsPage(): JSX.Element {
               );
             })}
 
-            <Dialog>
-              <DialogTrigger
-                onClick={async () => {
-                  await fetchImageLibrary();
-                }}
-              >
-                <div
-                  className={
-                    "flex min-h-60 w-[22.4rem] cursor-pointer items-center justify-center rounded-xl bg-red-100"
-                  }
-                >
-                  <div className={"rounded-full bg-red-300 p-4"}>
-                    <DynamicIcon icon={"PlusIcon"} className={"text-red-800"} />
-                  </div>
-                </div>
-              </DialogTrigger>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle asChild={true}>
-                    <div className={"space-y-3"}>
-                      <h1>Image Library</h1>
-                      <p className={"text-xs text-gray-400"}>
-                        Click on the image to add it.
-                      </p>
-                    </div>
-                  </DialogTitle>
-                  <DialogDescription
-                    asChild={true}
-                    className={"max-h-72 overflow-y-auto pt-10"}
-                  >
-                    {imageLibraryLoading ? (
-                      <Fragment>Loading...</Fragment>
-                    ) : imageLibrary !== null && imageLibrary !== undefined ? (
-                      <div className={"grid grid-cols-3 gap-4"}>
-                        {imageLibrary.map((image) => {
-                          return (
-                            <DialogClose
-                              key={image}
-                              onClick={() => {
-                                dispatch({
-                                  type: "SET_CONFIG",
-                                  payload: {
-                                    config: {
-                                      ...remoteConfig,
-                                      images: {
-                                        ...remoteConfig.images,
-                                        carousel: [
-                                          ...remoteConfig.images.carousel,
-                                          {
-                                            url: image,
-                                            description: "Image from Library"
-                                          }
-                                        ]
-                                      }
-                                    }
-                                  }
-                                });
-                              }}
-                            >
-                              <Image
-                                src={image}
-                                alt={"Image"}
-                                width={200}
-                                height={100}
-                                className={"h-full w-full rounded-xl"}
-                              />
-                            </DialogClose>
-                          );
-                        })}
-                      </div>
-                    ) : (
-                      <Fragment>Cannot load Images</Fragment>
-                    )}
-                  </DialogDescription>
-                </DialogHeader>
-              </DialogContent>
-            </Dialog>
+            <div
+              className={
+                "flex min-h-60 w-[22.4rem] cursor-pointer items-center justify-center rounded-xl bg-red-100"
+              }
+              onClick={async () => {
+                await fetchImageLibrary();
+                setShowImagePicker(true);
+              }}
+            >
+              <div className={"rounded-full bg-red-300 p-4"}>
+                <DynamicIcon icon={"PlusIcon"} className={"text-red-800"} />
+              </div>
+            </div>
           </div>
         </div>
       </section>
