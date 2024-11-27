a3541f2375779c1a832f9eb32e4d31c633373091
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-11-27 13:15:42 +0530

    build (puconvocation-com): Static assets are now served by CloudFront CDN instead of being served by the next.js server.

--- frontend/websites/puconvocation-com/next.config.ts
+++ frontend/websites/puconvocation-com/next.config.ts
@@ -15,47 +15,54 @@ import createNextIntlPlugin from "next-intl/plugin";
 import withPlaiceholder from "@plaiceholder/next";
 import { NextConfig } from "next";
 import bundleAnalyzer from "@next/bundle-analyzer";
+import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
 
-const i18n = createNextIntlPlugin();
+const server = (phase: any) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+  const i18n = createNextIntlPlugin();
 
-const nextConfig: NextConfig = {
-  output: "standalone",
-  poweredByHeader: false,
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "assets.puconvocation.com",
-      },
-    ],
-  },
-  experimental: {
-    optimizePackageImports: [
-      "@components/analytics",
-      "@components/attendee",
-      "@components/common",
-      "@components/forms",
-      "@components/graphics",
-      "@components/layouts",
-      "@components/ui",
-      "@controllers",
-      "@hooks",
-      "@dto",
-      "@enums",
-      "@fonts",
-      "@providers",
-      "@services",
-      "@i18n",
-      "@lib",
-      "react-zxing",
-    ],
-  },
+  const nextConfig: NextConfig = {
+    output: "standalone",
+    poweredByHeader: false,
+    reactStrictMode: true,
+    assetPrefix: isDev ? undefined : "https://assets.puconvocation.com",
+    images: {
+      unoptimized: true,
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "assets.puconvocation.com",
+        },
+      ],
+    },
+    experimental: {
+      optimizePackageImports: [
+        "@components/analytics",
+        "@components/attendee",
+        "@components/common",
+        "@components/forms",
+        "@components/graphics",
+        "@components/layouts",
+        "@components/ui",
+        "@controllers",
+        "@hooks",
+        "@dto",
+        "@enums",
+        "@fonts",
+        "@providers",
+        "@services",
+        "@i18n",
+        "@lib",
+        "react-zxing",
+      ],
+    },
+  };
+
+  const withBundleAnalyzer = bundleAnalyzer({
+    enabled: process.env.ANALYZE === "true",
+  });
+
+  return withBundleAnalyzer(withPlaiceholder(i18n(nextConfig)));
 };
 
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-});
-
-export default withBundleAnalyzer(withPlaiceholder(i18n(nextConfig)));
+export default server;
